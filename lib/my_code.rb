a3ee405b48a10ebc57c5511a16a786d0d214b1a1
Chance Mims <chancemims1@gmail.com>
2019-12-05 19:22:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,19 @@
 # Your Code Here
+def generalized_map(source_array)
+  result_array = []
+  counter_index = 0
+  while counter_index < source_array.length do
+    result_array << yield(source_array[counter_index])
+    counter_index += 1
+  end
+  return result_array
+end
+
+def generalized_redux(source_array, starting_value = 0)
+  array_total = starting_value
+  counter_index = 0
+  while counter_index < source_array.length do
+    array_total += yield(source_array[counter_index])
+    counter_index += 1
+  end
+end
